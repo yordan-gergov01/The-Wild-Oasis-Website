cd40530b581c53cf54e23fd84228b341e1534152
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-03 18:05:49 +0200

    adding nested layout

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,5 +1,9 @@
 import ReservationCard from "../../_components/ReservationCard";
 
+export const metadata = {
+  title: "Reservations",
+};
+
 export default function Page() {
   // CHANGE
   const bookings = [];
