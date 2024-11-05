a034fd40cbc2dfbf6fdeb63211415a89b506cccd
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-05 09:11:26 +0200

    building the fitler feature for cabins and passing data between client and server components with url

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 3600;
 
@@ -8,7 +9,10 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+//searchParams are only available here in the page.js file (and others page.js), pass the data from client to server component
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -26,8 +30,12 @@ export default async function Page() {
       {/* We apply Suspense here, because the above paragraph is independent of the
       data fetching for cabins */}
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
