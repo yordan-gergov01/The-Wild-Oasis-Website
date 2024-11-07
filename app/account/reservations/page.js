baff0e9fe28f45335bad6c6f89a16748d010b5a8
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-07 09:32:12 +0200

    building the guest's reservations page

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import ReservationCard from "../../_components/ReservationCard";
+import { getBookings } from "@/app/_lib/data-service";
+import { auth } from "@/app/_lib/auth";
 
 export const metadata = {
   title: "Reservations",
 };
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function Page() {
+  const session = await auth();
+  const bookings = await getBookings(session.user.guestId);
 
   return (
     <div>
